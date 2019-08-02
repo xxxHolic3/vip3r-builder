59f97cfa0e088f4f45db429f10bbab60784f3f4b
xxxHolic3 <33705429+xxxHolic3@users.noreply.github.com>
2019-08-03 00:44:16 +0700

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -2,30 +2,30 @@
 clear
 echo "
 ==============================================
-|          VIP3r-OS Builder Script           |
+|          Paranoid-OS Builder Script        |
 |                                            |
-|   Version  : 2.4 Beta                      |
-|   Codename : benatar                       |
+|   Version  : 3.0 Beta                      |
+|   Codename : Memories                      |
 |   Author   : vip3r                         |
 |   Desc.    : This script for building      |
-|              VIP3r-OS based on debian      |
-|              unstable using live-build     |
+|              Paranoid-OS based on debian   |
+|              unstable using live-build.    |
 ==============================================
 
 "
 echo -e "This script must be run with sudo/root permissions\n"
 
 #step 1 - pre
-echo -e "Checking vip3r-material directory..."
-if [-d "vip3r-material/"]
+echo -e "Checking paranoid-material directory..."
+if [-d "paranoid-material/"]
 then
 	echo -e "directory found...\n"
 else
 	echo -e "directory does not exist...\n"
-	echo -e "create vip3r-material directory...\n"
-	mkdir -p viper-material/{desktop-config, backgrounds, scripts, icons, hooks, bootloaders, misc64}
+	echo -e "create paranoid-material directory...\n"
+	mkdir -p paranoid-material/{desktop-config, backgrounds, scripts, icons, hooks, bootloaders, misc64}
 	echo -e "Done...\n"
-	echo -e "Puts your modifications file to vip3r-material directory\n"
+	echo -e "Puts your modifications file to paranoid-material directory\n"
 fi
 #================================== end step 1 =====================================
 
@@ -34,8 +34,8 @@ fi
 lct="$(pwd)"
 
 #create build directory
-mkdir vip3r
-cd vip3r
+mkdir paranoid
+cd paranoid
 
 #setup live-build config
 lb config	--binary-images iso-hybrid\
@@ -44,22 +44,25 @@ lb config	--binary-images iso-hybrid\
 			--linux-flavours amd64\
 			--distribution unstable\
 			--archives-areas "main contrib non-free"\
-			--updates true\
-			--security true\
+			--mirror-bootstrap "http://kartolo.sby.datautama.net.id/debian"\
+			--updates false\
+			--security false\
+			--backports false\
 			--cache true\
 			--apt-recommend true\
-			--debian-installer live\
-			--debian-installer-gui false\
+			--bootappend-live "boot=live username=root hostname=paranoid noautomount"
+			--debian-installer-gui live\
 			--win32-loader false\
-			--iso-application vip3r-OS_1.1beta\
+			--iso-application Paranoid-OS_1.0beta\
 			--iso-publisher https://github.com/xxxHolic3\
-			--iso-volume vip3r-OS_1.1beta-live
+			--iso-volume Paranoid-OS_1.0beta-live
 
 #added Mate desktop manager
-echo task-mate-desktop > $lct/vip3r/config/package-lists/desktop.list.chroot
+echo task-mate-desktop > $lct/paranoid/config/package-lists/desktop.list.chroot
 
 #added default tools installed
 echo	gparted\
+		linux-image\
 		vlc\
 		firefox-esr\
 		squashfs-tools\
@@ -80,8 +83,7 @@ echo	gparted\
 		apt-transport-https\
 		dirmngr\
 		firmware-linux\
-		> $lct/vip3r/config/package-lists/package-list.chroot
+		> $lct/paranoid/config/package-lists/package-list.chroot
 
 #copying meta-vip3r file from vip3r-material directory
-cp $lct/vip3r-material/metapackages/metavip3r-list.chroot > $lct/vip3r/config/package-lists/metavip3r-list.chroot
-
+cp $lct/paranoid-material/metapackages/paranoidMeta-list.chroot > $lct/paranoid/config/package-lists/paranoidMeta-list.chroot
